ab878807b464d733f721bda029442312affeff57
Karlysson Alves <karlysson.alves@gmail.com>
2019-08-10 15:25:40 -0300

    Project with devise

---
+++ db/migrate/20190810180746_devise_create_users.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+
+      ## My Dados
+      t.string :name, null: false, comment: 'Nome'
+      t.string :description, null: false, comment: 'Descrição'
+
+      ## Database authenticatable
+      t.string :email,              null: false, default: '', comment: 'Email'
+      t.string :encrypted_password, null: false, default: '', comment: 'Senha Criptografada'
+
+      ## Recoverable
+      t.string   :reset_password_token, comment: 'Token do Reset Senha'
+      t.datetime :reset_password_sent_at, comment: 'Data que o Reset Senha, foi enviado'
+
+      ## Rememberable
+      t.datetime :remember_created_at, comment: 'Data de Criação do Remember'
+
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.inet     :current_sign_in_ip
+      # t.inet     :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20190810180746) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                                null: false, comment: "Nome"
+    t.string   "description",                         null: false, comment: "Descrição"
+    t.string   "email",                  default: "", null: false, comment: "Email"
+    t.string   "encrypted_password",     default: "", null: false, comment: "Senha Criptografada"
+    t.string   "reset_password_token",                             comment: "Token do Reset Senha"
+    t.datetime "reset_password_sent_at",                           comment: "Data que o Reset Senha, foi enviado"
+    t.datetime "remember_created_at",                              comment: "Data de Criação do Remember"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+end
