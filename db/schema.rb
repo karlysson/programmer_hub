63e274ff6718135183f94dd05566ea00822076f0
Karlysson Alves <karlysson.alves@gmail.com>
2019-08-13 22:45:37 -0300

    Likes + Comments

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,22 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_814_010_133) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 20190814014305) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name',                                null: false, comment: 'Nome'
-    t.string   'description',                         null: false, comment: 'Descrição'
-    t.string   'email',                  default: '', null: false, comment: 'Email'
-    t.string   'encrypted_password',     default: '', null: false, comment: 'Senha Criptografada'
-    t.string   'reset_password_token',                             comment: 'Token do Reset Senha'
-    t.datetime 'reset_password_sent_at',                           comment: 'Data que o Reset Senha, foi enviado'
-    t.datetime 'remember_created_at',                              comment: 'Data de Criação do Remember'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.string   'avatar'
-    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title",            limit: 50, default: ""
+    t.text     "comment"
+    t.string   "commentable_type"
+    t.integer  "commentable_id"
+    t.integer  "user_id"
+    t.string   "role",                        default: "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+    t.index ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
+
+  create_table "follows", force: :cascade do |t|
+    t.string   "followable_type",                 null: false
+    t.integer  "followable_id",                   null: false
+    t.string   "follower_type",                   null: false
+    t.integer  "follower_id",                     null: false
+    t.boolean  "blocked",         default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["followable_id", "followable_type"], name: "fk_followables", using: :btree
+    t.index ["follower_id", "follower_type"], name: "fk_follows", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                                null: false, comment: "Nome"
+    t.string   "description",                         null: false, comment: "Descrição"
+    t.string   "email",                  default: "", null: false, comment: "Email"
+    t.string   "encrypted_password",     default: "", null: false, comment: "Senha Criptografada"
+    t.string   "reset_password_token",                             comment: "Token do Reset Senha"
+    t.datetime "reset_password_sent_at",                           comment: "Data que o Reset Senha, foi enviado"
+    t.datetime "remember_created_at",                              comment: "Data de Criação do Remember"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
 end
